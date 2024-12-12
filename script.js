c8e0dd95a7964ee58dbe8228f6acc7403925138b
Thomas Vidal <129500408+MrGriPy@users.noreply.github.com>
2024-12-12 10:57:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const revenueChart = new Chart(revenueCtx, {
                 borderColor: 'rgba(102, 51, 153, 1)',
                 fill: true,
                 tension: 0.4,
-                pointRadius: 5 // Taille des points
+                pointRadius: 8 // Taille des points
             },
             {
                 label: 'Console  ',
